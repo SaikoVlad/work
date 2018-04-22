ace67d6b490de156d57826061f48b55828afb91b
prostfk@gmail.com <prostfk@gmail.com>
2018-04-22 23:16:40 +0300

    c setting

--- Java/Java EE/Spring/SpringTutorials/ItNews/src/main/java/by/bntu/fitr/povt/prostrmk/ItNews/model/script.sql
+++ Java/Java EE/Spring/SpringTutorials/ItNews/src/main/java/by/bntu/fitr/povt/prostrmk/ItNews/model/script.sql
@@ -1,11 +1,7 @@
-# CREATE TABLE ItNews.Articles (
-#  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-#  type varchar(20),
-#  title varchar(255),
-#  content text,
-#  pathToFile VARCHAR(255)
-# )
-DELETE FROM Articles WHERE id = 5;
-# DELETE FROM Articles WHERE id = 2;
-# DELETE FROM Articles WHERE id = 3;
-# DELETE FROM Articles WHERE id = 4;
+ CREATE TABLE ItNews.Articles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  type varchar(20),
+  title varchar(255),
+  content text,
+  pathToFile VARCHAR(255)
+ )
