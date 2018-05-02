e710772642928b000fc208490818b5b223fc2ac2
prostfk@gmail.com <prostfk@gmail.com>
2018-05-02 23:02:09 +0300

    fixing resolvers

--- Java/Java EE/Spring/SpringTutorials/ItNews/src/main/java/by/bntu/fitr/povt/prostrmk/ItNews/config/WebMvcConfig.java
+++ Java/Java EE/Spring/SpringTutorials/ItNews/src/main/java/by/bntu/fitr/povt/prostrmk/ItNews/config/WebMvcConfig.java
@@ -1,20 +1,24 @@
 package by.bntu.fitr.povt.prostrmk.ItNews.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(
-                "/css/**",
-                "/js/**")
-                .addResourceLocations(
-                        "classpath:/resource/static/css/",
-                        "classpath:/resource/static/js/");
-    }
-
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        registry.addResourceHandler("/css/**").addResourceLocations("/resource/static/css/").setCachePeriod(31556926);
+//
+//    }
+//
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//        configurer.enable();
+//    }
 }
