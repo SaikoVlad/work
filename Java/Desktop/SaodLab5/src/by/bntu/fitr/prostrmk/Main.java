235a51af1588ad9c41cf6ef4aa1b529f553507dc
prostfk@gmail.com <prostfk@gmail.com>
2018-05-06 15:55:56 +0300

    add search

--- Java/Desktop/SaodLab5/src/by/bntu/fitr/prostrmk/Main.java
+++ Java/Desktop/SaodLab5/src/by/bntu/fitr/prostrmk/Main.java
@@ -6,7 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        CustomArray array = new CustomArray(1,2,3,4,5,6);
+        array.binarySearch(4);
+        System.out.println(array.linearSearch(4));
 
 
     }
